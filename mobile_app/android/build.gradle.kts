c059ecc82f103b8980e10d548eac14498482d459
tayarieya <tayarieya@users.noreply.github.com>
2025-12-16 20:07:28 +0100

    fix: Simplify build.gradle.kts and update plugin versions

--- mobile_app/android/build.gradle.kts
+++ mobile_app/android/build.gradle.kts
@@ -5,15 +5,10 @@ allprojects {
     }
 }
 
-val newBuildDir: Directory =
-    rootProject.layout.buildDirectory
-        .dir("../../build")
-        .get()
-rootProject.layout.buildDirectory.value(newBuildDir)
+rootProject.layout.buildDirectory = rootProject.layout.buildDirectory.dir("../build")
 
 subprojects {
-    val newSubprojectBuildDir: Directory = newBuildDir.dir(project.name)
-    project.layout.buildDirectory.value(newSubprojectBuildDir)
+    project.layout.buildDirectory = rootProject.layout.buildDirectory.get().dir(project.name)
 }
 
 tasks.register<Delete>("clean") {
